b41d63f3b326e64e0a461a9598a0a8eb908b9eeb
Aashutosh <aashutosh.gupta@zuru.com>
2024-06-05 15:08:00 +0530

    update consent page

--- src/Consent.jsx
+++ src/Consent.jsx
@@ -3,17 +3,25 @@ import './ConsentForm.css'; // Import CSS file for styling
 
 const ConsentForm = () => {
     const [consentChallenge, setConsentChallenge] = useState('');
+    const [scope, setScope] = useState('');
+    const [appName, setAppName] = useState('');
   
     useEffect(() => {
       const searchParams = new URLSearchParams(window.location.search);
       const consent_challenge = searchParams.get('consent_challenge');
+      const scopeValue = searchParams.get('scope');
+      const appNameValue = searchParams.get('appName');
       setConsentChallenge(consent_challenge);
+      setScope(scopeValue);
+      setAppName(appNameValue);
     }, []);
+
+
 
   return (
     <>
     <div className="consent-container">
-            <h1>Authorize An App</h1>
+            <h1>Authorize {appName} App</h1>
             <div className="form-container">
       <form className="form-consent" method="post" action="http://localhost:8000/consent">
       <input type="hidden" name="consent_challenge" value={consentChallenge}/>
@@ -21,7 +29,7 @@ const ConsentForm = () => {
         <div className="form-check">
             <input type="checkbox" name="grant_scope" id="offline-checkbox" value="offline" checked />
             <input type="hidden" name="grant_scope" value="offline" />
-            <label htmlFor="offline-checkbox" className="form-check-label">Offline Access</label>
+            <label htmlFor="offline-checkbox" className="form-check-label">{scope}</label>
         </div>
         <button className="btn btn-primary" type="submit">Authorize</button>
       </form>
